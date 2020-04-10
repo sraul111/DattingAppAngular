90ef1f60b17c437dfd40d878ffa62f414076e716
Sujit Raul <sraul111@gmail.com>
2020-04-10 13:58:23 +0000

    initial commit

---
+++ DattingApp.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using DattingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DattingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public  DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<Value> Values {get;set;}
+    }
+}
